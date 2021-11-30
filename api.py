f6202a32449cadf0132e9b13c8778aa992c57c37
viktoriastash <89913068+viktoriastash@users.noreply.github.com>
2021-11-30 18:18:51 +0200

    Update api.py

--- api.py
+++ api.py
@@ -194,3 +194,28 @@ class PetFriends:
             result = res.text
         print(result)
         return status, result
+
+    def add_new_pet_simple(self, auth_key: json, name: str, animal_type: str,
+                           age: str) -> json:
+        """Метод отправляет (постит) на сервер данные о добавляемом питомце
+        в простом виде(без фотографии) и возвращает статус
+        запроса на сервер, и результат в формате JSON с данными добавленного питомца"""
+
+        data = MultipartEncoder(
+            fields={
+                'name': name,
+                'animal_type': animal_type,
+                'age': age,
+
+            })
+        headers = {'auth_key': auth_key['key'], 'Content-Type': data.content_type}
+
+        res = requests.post(self.base_url + 'api/create_pet_simple', headers=headers, data=data)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        print(result)
+        return status, result
